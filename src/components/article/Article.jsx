776e8f5a87f217fda6269e2800d77c42dc642f3d
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-04-16 00:04:50 +0300

    Updated

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,7 +2,12 @@
 
 function Article() {
   return (
-    <div></div>
+    <div className="article-wrapper flex justify-center my-36">
+      <article className="experience-wrapper">
+        <h2 className="inter-extrabold text-white text-5xl tracking-tight">EXPERIENCE</h2>
+        <p className="inter-medium bg-gradient-to-r from-[#9C83FF] to-[#FF9051] text-transparent bg-clip-text text-xs tracking-[.70em] text-center">EXPLORE NOW</p>
+      </article>
+    </div>
   )
 }
 
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -20,7 +20,7 @@ const Section = () => {
                     <span className='text-[26px] poppins-semibold'>🍿</span>
                 </div>
                 <div className='w-[587px]'>
-                    <p className='text-[#E1E1E1] text-[16px] poppins-medium leading-6 mt-3'>Passionate Computer Programmer with a focus on MERN, PHP and Flutter development,
+                    <p className='text-[#E1E1E1] text-[16px] poppins-medium leading-6 mt-3 text-center'>Passionate Computer Programmer with a focus on MERN, PHP and Flutter development,
                         dedicated to crafting elegant and user-friendly web and mobile applications.</p>
                 </div>
                 <div className="border-[#585858] border rounded-[75px] w-[187px] h-[55px] flex justify-center mt-8">
@@ -34,6 +34,14 @@ const Section = () => {
                 <div>
                     <p className="inter-medium bg-gradient-to-r from-[#9C83FF] to-[#FF9051] text-transparent bg-clip-text text-xs tracking-[.70em]">EXPLORE NOW</p>
                 </div>
+                <div className="w-[706px] h-auto min-h-[147px] max-h-[300px] overflow-auto leading-5 mt-5 px-10">
+                    <p className="text-[#E1E1E1] text-center inter-medium text-base">
+                        As a passionate computer programmer, I thrive on the intricate dance between logic and creativity. Currently immersed in the dynamic world of PHP, C++, Python, MERN Stack my expertise centers around Flutter, where I seamlessly blend technology with innovation.
+                        <br /><br />
+                        With a fervor for crafting elegant solutions, I navigate the ever-evolving landscape of software development. My journey involves translating concepts into code, creating seamless user experiences, and constantly pushing the boundaries of what's possible.
+                    </p>
+                </div>
+
             </section>
         </div>
     )
