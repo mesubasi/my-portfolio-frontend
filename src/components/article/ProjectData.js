7c7ee6e1ba86b3b2fd1ca24706ce93fc1554f5f0
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-12 22:33:41 +0300

    ProjectData.js Added

---
+++ src/components/article/ProjectData.js
@@ -0,0 +1,36 @@
+const Project = [
+  {
+    id: "1",
+    projectImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMAHGby2vJCooaOrD5jX8BchRcDw7ypUQOw&s",
+    projectName: "WebHR",
+    projectDescription:
+      "Designed a modern UI website comprising more than 50 screens, along with the integration of a blog using Next.js.",
+  },
+  {
+    id: "2",
+    projectImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMAHGby2vJCooaOrD5jX8BchRcDw7ypUQOw&s",
+    projectName: "WebHR",
+    projectDescription:
+      "Designed a modern UI website comprising more than 50 screens, along with the integration of a blog using Next.js.",
+  },
+  {
+    id: "3",
+    projectImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMAHGby2vJCooaOrD5jX8BchRcDw7ypUQOw&s",
+    projectName: "WebHR",
+    projectDescription:
+      "Designed a modern UI website comprising more than 50 screens, along with the integration of a blog using Next.js.",
+  },
+  {
+    id: "4",
+    projectImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIMAHGby2vJCooaOrD5jX8BchRcDw7ypUQOw&s",
+    projectName: "WebHR",
+    projectDescription:
+      "Designed a modern UI website comprising more than 50 screens, along with the integration of a blog using Next.js.",
+  },
+];
+
+export default Project;
