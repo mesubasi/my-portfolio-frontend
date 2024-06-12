e3f1bb5d756e6596f0be88c0f94a63cb9cea7340
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-12 22:41:19 +0300

    Data.js Edited

--- src/components/article/Data.js
+++ src/components/article/Data.js
@@ -1,25 +1,25 @@
 const Data = [
   {
     id: 1,
-    sirketAdi: 'GAME AND APPLICATION ACADEMY',
-    pozisyonAdi: 'Trainee',
-    calismaTarihi: 'NOV 2023',
-    devamEdiyor: 'PRESENT',
-    konum: 'BURSA',
-    aciklama:
-      'With the support provided by Google, Ministry of Industry and Technology, Presidency Digital Transformation Office, Entrepreneurship Foundation and T3 Initiative Centre, I am actively working in the field of Mobile Application Development in the project within the scope of Game and Application Development. In this exciting project, we focus on valuable skills such as project management and technology entrepreneurship, as well as mobile application development with leading technologies such as Dart and Flutter. This experience allows me to not only develop my technical skills but also gain a deep understanding of project management and technology entrepreneurship. With these various skills provided by the project, I am not only strengthening my pioneering position in the industry, but also increasing my passion for developing creative and effective mobile applications. The experience I have gained in this process has contributed greatly to my development not only in the world of technology, but also in the areas of successful management of projects and entrepreneurship.',
-    kullanilanlar: ['Guide'],
+    companyName: "GAME AND APPLICATION ACADEMY",
+    positionName: "Trainee",
+    workDate: "NOV 2023",
+    Status: "PRESENT",
+    Location: "BURSA",
+    Description:
+      "With the support provided by Google, Ministry of Industry and Technology, Presidency Digital Transformation Office, Entrepreneurship Foundation and T3 Initiative Centre, I am actively working in the field of Mobile Application Development in the project within the scope of Game and Application Development. In this exciting project, we focus on valuable skills such as project management and technology entrepreneurship, as well as mobile application development with leading technologies such as Dart and Flutter. This experience allows me to not only develop my technical skills but also gain a deep understanding of project management and technology entrepreneurship. With these various skills provided by the project, I am not only strengthening my pioneering position in the industry, but also increasing my passion for developing creative and effective mobile applications. The experience I have gained in this process has contributed greatly to my development not only in the world of technology, but also in the areas of successful management of projects and entrepreneurship.",
+    skills: ["Guide"],
   },
   {
     id: 2,
-    sirketAdi: 'PWNLAB.ME',
-    pozisyonAdi: 'Content Creator',
-    calismaTarihi: 'JUN 2023',
-    devamEdiyor: 'PRESENT',
-    konum: 'BURSA',
-    aciklama:
-      'I pursue my passion for cybersecurity and technology as a freelance content producer on PwnLab. Currently, I have been active in the PwnLab.me community since June 2023. During this time, I have been utilising my editorial and content management skills to provide valuable content to the community.\n\nMy mission at PwnLab.me is to share my knowledge on cyber security and technology and contribute to a community that follows developments in this field. As a content creator, it is an exciting challenge for me to explain complex topics in an understandable way through written and visual media. With my editorial and content management skills, I contribute to the growth of the community by engaging readers.\n\nKeeping up with the rapid changes in the world of cyber security and technology, I continue to produce unique content on PwnLab.me. Using my skills in editing and content management, I contribute to the community to share relevant and up-to-date information in this field. With my passion for gathering and sharing creative content, my adventure at PwnLab.me continues.',
-    kullanilanlar: ['Snort', 'Cyber Security', 'CyberOps'],
+    companyName: "PWNLAB.ME",
+    positionName: "Content Creator",
+    workDate: "JUN 2023",
+    Status: "PRESENT",
+    Location: "BURSA",
+    Description:
+      "I pursue my passion for cybersecurity and technology as a freelance content producer on PwnLab. Currently, I have been active in the PwnLab.me community since June 2023. During this time, I have been utilising my editorial and content management skills to provide valuable content to the community.\n\nMy mission at PwnLab.me is to share my knowledge on cyber security and technology and contribute to a community that follows developments in this field. As a content creator, it is an exciting challenge for me to explain complex topics in an understandable way through written and visual media. With my editorial and content management skills, I contribute to the growth of the community by engaging readers.\n\nKeeping up with the rapid changes in the world of cyber security and technology, I continue to produce unique content on PwnLab.me. Using my skills in editing and content management, I contribute to the community to share relevant and up-to-date information in this field. With my passion for gathering and sharing creative content, my adventure at PwnLab.me continues.",
+    skills: ["Snort", "Cyber Security", "CyberOps"],
   },
 ];
 
